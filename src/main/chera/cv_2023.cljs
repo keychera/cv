ba480e9d204f8cbc8d96d22888ac20d7d56aa706
keychera <kyogatama@gmail.com>
2023-10-15 11:18:53 +0700

    update wording

--- src/main/chera/cv_2023.cljs
+++ src/main/chera/cv_2023.cljs
@@ -10,8 +10,9 @@
 
 (def content
   {:name "Kevin Erdiza Yogatama"
-   :about "I am a software engineer with an experience in a test automation engineering team.
-Within my job, I have been exploring backend solutions and implementing them to solve internal teams needs."
+   :about [:<>
+           [:p "I am a software engineer with an experience in a Test Automation. Within my job, I also explored and implemented backend solutions to solve testing needs."]
+           [:p "I can create test automation from either existing stack or building one from scratch. I can also cooperate across different team and I am the kind of person who takes initiative to solve problems."]]
    :profile-url "me_2019.png"
    :contact
    [{:name "twitter", :display "@keychera", :link "https://twitter.com/keychera"}
@@ -28,35 +29,36 @@ Within my job, I have been exploring backend solutions and implementing them to 
        :items
        [{:type "project", :title "I proposed and developed a pipeline/queue system to aid automated test execution"
          :desc
-         ["Custom pipeline/queue server to compile and execute kotlin codebase."
-          "A test report aggregator, small-scale internal dashboard, and web helper tools."
-          "Web development that focus on the backend side by lowering front-end development with HTMX and Bootstrap."
-          "Integrated the server with several development tools such as Gitlab scheduling and Slack."]
+         ["I implemented custom pipeline/queue server to compile and execute kotlin test scripts."
+          "I implemented a test report aggregator, small-scale internal dashboard, and web helper tools in the same server."
+          "I decided on a stack that focus on the backend side by lowering front-end development effort with HTMX and Bootstrap."
+          "I integrated the server with several development tools such as Gitlab scheduling and Slack."]
          :tools ["Clojure" "HTMX" "SQLite" "Bootstrap" "Kubernetes" "GCP" "Oauth2-proxy"]}
-        {:type "project", :title "Developed a custom testing framework for QA team"
+        {:type "project", :title "I developed and maintain a custom testing framework for my QA team"
          :desc
-         ["Drove test script standardization, configurability, and readability to the team."
-          "Dealt with code distribution (library publishing via Gitlab registry)."
-          "Simplified the build system for test scripts (Maven pom.xml generator)."
-          "Built a base framework that is reusable for multiple entities."
-          "Mentored and trained other member to use and help contribute to this framework"]
+         ["I drove test script standardization, configurability, and readability to the team."
+          "I dealt with code distribution (library publishing via Gitlab registry)."
+          "I simplified the build system for test scripts (Maven pom.xml generator)."
+          "I built a base framework that is reusable for multiple entities."
+          "I trained other member to use and help contribute to this framework."]
          :tools ["Kotlin" "Junit5" "Selenium" "Appium" "RestAssured" "GRPC/Protobuf" "Maven" "Gradle" "Clojure/Babashka" "Gitlab CI"]}]}
       {:title "Junior Test Automation Engineer"
        :extra "Oktober 2020  - April 2022"
        :items
-       [{:type "project", :title "Introduced Kotlin usage for test scripting"
-         :desc ["Proposed Kotlin to write test scripts to the team."
-                "Orchestrate code migration and train other members to use Kotlin."]
+       [{:type "project", :title "I introduced Kotlin and spearheaded migration to Kotlin for test scripting"
+         :desc ["I proposed Kotlin to write test scripts to the team."
+                "I orchestrated code migration and trained other members to use Kotlin for testing."]
          :tools ["Kotlin" "Gradle"]}
-        {:type "project", :title "Write code generators to help GRPC/Protobuf testing"
-         :desc ["Wrote a code generator based on the protobuf definition and the officialy-generated Java code to simplify writing test scripts."]
+        {:type "project", :title "I wrote several code generators to help GRPC/Protobuf testing."
+         :desc ["I wrote a code generator that transform protobuf definition into a helpful code wrapper to simplify writing GRPC testing"]
          :tools ["Java" "Kotlin" "GRPC/Protobuf"  "Gradle"]}
-        {:type "project", :title "Write wrapper library to standardize test scripts"
-         :desc ["Designed an abstraction that standardize test scripts and introduced them to the team."
-                "Dealt with code distribution with maven-publishing and Gitlab registry."]
+        {:type "project", :title "I wrote test automation scripts and implemented a number of wrapper"
+         :desc ["I wrote and maintain test scripts withing Java and Junit5 stack."
+                "I designed an abstraction that standardize test scripts and introduced them to the team that is still used until now."
+                "I dealt with code distribution with Gradle, Maven-publishing, and Gitlab registry."]
          :tools ["Java" "Selenium" "Appium" "RestAssured" "Gradle"]}
         {:type "project", :title "Write load test scripts"
-         :desc ["Wrote scripts using Gatling, write Dockerfiles, and help preparing load test environment."]
+         :desc ["I wrote load test scripts using Gatling, and help preparing load test environment with Docker and Kubernetes."]
          :tools ["Scala" "Docker" "Kubernetes" "GCP" "Gitlab CI"]}]}]}]
    :education
    [{:title "Bandung Institute of Technology"
@@ -67,11 +69,11 @@ Within my job, I have been exploring backend solutions and implementing them to 
    [{:title "JVM Languages"
      :items ["Kotlin" "Clojure" "Java"]}
     {:title "Server"
-     :items ["http-kit" "Ring-Jetty"]}
+     :items ["Ring-Jetty" "http-kit"]}
     {:title "Deployment"
      :items ["Docker" "Kubernetes" "GCP" "Gitlab CI"]}
     {:title "Databases"
-     :items ["MySQL" "SQLite"]}
+     :items ["SQLite" "MySQL"]}
     {:title "Testing"
      :items ["Junit5" "Selenium" "Appium" "RestAssured"]}
     {:title "Front-end"
@@ -86,11 +88,11 @@ Within my job, I have been exploring backend solutions and implementing them to 
      [:br] "I have been practicing weekly using Italki"
      [:br] (Link {:href "https://www.italki.com/user/18089807"})]]
    :extra
-   [[:<> "Create an open source tool for Hot-reloading Clojure/Babahska + HTMX web projects." [:br]
+   [[:<> "I created an open source tool for Hot-reloading Clojure/Babahska + HTMX web projects." [:br]
      "repo: " (Link {:href "https://github.com/keychera/panas.reload"})]
-    [:<> "Create a Japanese Number converter, language learning tool webapp." [:br]
-     "web: " (Link {:href "https://keychera.github.io/kazoeru/"})] 
-    [:<> "This very CV, originally a React project, converted to ClojureScript/Reagent." [:br]
+    [:<> "I created a Japanese Number converter, language learning tool webapp." [:br]
+     "web: " (Link {:href "https://keychera.github.io/kazoeru/"})]
+    [:<> "I created this very CV as a static web, which is originally a React project, but converted to ClojureScript/Reagent." [:br]
      "web: " (Link {:href "https://keychera.github.io/cv/2023"})]]})
 
 (defn CenterTitle [title]
@@ -106,19 +108,17 @@ Within my job, I have been exploring backend solutions and implementing them to 
      [:div [:> Text {:code true} type] "・" time]]
     [:> Col {:span 6} extra]]])
 
-(defn Profile
-  ([] (Profile {}))
-  ([{:keys []}]
-   [:> Card
-    {:actions (->> content :contact
-                   (mapv (fn [{:keys [name display link]}]
-                           (as-element
-                            [:> Text
-                             [:> Text {:strong true} name]
-                             (Link {:href link :children display})]))))}
-    [:div
-     [:h3 (:name content)]
-     [:p {:style {:color "#00000073"}} (:about content)]]]))
+(defn Profile []
+  [:> Card
+   {:actions (->> content :contact
+                  (mapv (fn [{:keys [name display link]}]
+                          (as-element
+                           [:> Text
+                            [:> Text {:strong true} name]
+                            (Link {:href link :children display})]))))}
+   [:div
+    [:h3 (:name content)]
+    [:p {:style {:color "#00000073"}} (:about content)]]])
 
 (defn Educations []
   [:<>
@@ -207,25 +207,24 @@ Within my job, I have been exploring backend solutions and implementing them to 
   [:<>
    (CenterTitle "Professional Experiences 💻")
    (->> (:projects content)
-       (map (fn [{job-items :items
-                  :keys [title subtitle extra]}]
-              [:div {:style {:padding "1rem"}}
-               [:div {:style {:display "flex" :align-items "center"}}
-                [:h3 title] [:div {:style {:margin-left "0.5rem"}} extra]]
-               [:div subtitle]
-               [:div
-                (->> job-items
-                     (map (fn [{proj-items :items
-                                :keys [title extra]}]
-                            [:<>
-                             [:> Space {:align "center"} "≫" [:h4 title] [:span extra]]
-                             (->> proj-items
-                                  (map-indexed ProjectCard))])))]])))])
+        (map (fn [{job-items :items
+                   :keys [title subtitle extra]}]
+               [:div {:style {:padding "1rem"}}
+                [:div {:style {:display "flex" :align-items "center"}}
+                 [:h3 title] [:div {:style {:margin-left "0.5rem"}} extra]]
+                [:div subtitle]
+                [:div
+                 (->> job-items
+                      (map (fn [{proj-items :items
+                                 :keys [title extra]}]
+                             [:<>
+                              [:> Space {:align "center"} "≫" [:h4 title] [:span extra]]
+                              (->> proj-items
+                                   (map-indexed ProjectCard))])))]])))])
 
 (defn cv []
   (set! (.. js/document -title) "keychera's 2023 CV")
-  (let [big-screen? (useMediaQuery (clj->js {:minWidth 1500}))
-        desktop?    (useMediaQuery (clj->js {:minWidth 1224}))
+  (let [desktop?    (useMediaQuery (clj->js {:minWidth 1224}))
         tablet?     (useMediaQuery (clj->js {:minWidth 900 :maxWidth 1224}))]
     [:<>
      (cond
@@ -233,11 +232,11 @@ Within my job, I have been exploring backend solutions and implementing them to 
        [:<>
         [:> Row {:align "center"}
          [:> Col {:span 6}
-          (Profile {:big-screen? big-screen?})
-          (Educations)
+          (Profile)
           (Language)
-          (Skills false)]
-         [:> Col {:span 18} 
+          (Skills false)
+          (Educations)]
+         [:> Col {:span 18}
           (Experiences)
           (Projects)]]]
 
@@ -245,25 +244,24 @@ Within my job, I have been exploring backend solutions and implementing them to 
        [:> Row {:align "center"}
         [:> Col {:span 8}
          (Profile)
-         (Educations)
          (Language)
-         (Skills true)]
-        [:> Col {:span 16} 
+         (Skills true)
+         (Educations)]
+        [:> Col {:span 16}
          (Experiences)
          (Projects)]]
 
        :else ;; smaller than `tablet?`
        [:<>
-        (Profile {:big-screen? big-screen?})
+        (Profile)
         [:> Tabs
          {:defaultActiveKey "2" :type "card" :centered true
           :items [{:label "📚" :key "1"
-                   :children (as-element [:<> (Educations) (Language) (Projects)])}
+                   :children (as-element [:<> (Language) (Educations)])}
                   {:label "💻" :key "2"
-                   :children (as-element [:<> 
-                                          (Experiences)])}
+                   :children (as-element [:<> (Experiences) (Projects)])}
                   {:label "🔧" :key "3"
-                   :children (as-element (Skills false))}]}]
+                   :children (as-element [:<> (Skills false)])}]}]
         [:> Card]])]))
 
 (defn root []
